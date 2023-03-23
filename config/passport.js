14b7475e992d9d39b079759721832802e122a8bb
brettschoff <brett.schoff@gmail.com>
2023-03-23 15:20:10 -0600

    Setup OAuth and User Model

--- config/passport.js
+++ config/passport.js
@@ -1,17 +1,32 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 //Require your User Model here!
-
+const UserModel = require('../models/user')
 // configuring Passport!
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_SECRET,
     callbackURL: process.env.GOOGLE_CALLBACK
   },
-  function(accessToken, refreshToken, profile, cb) {
+  async function(accessToken, refreshToken, profile, cb) {
     // a user has logged in via OAuth!
     // refer to the lesson plan from earlier today in order to set this up
 
+    try{
+      let user = await UserModel.findOne({googleId: profile.id})
+      if(user) return cb(null, user)
+      user = await UserModel.create({
+        name: profile.displayName,
+        googleId: profile.id,
+        email: profile.emails[0].value,
+        avatar: profile.photos[0].value
+      })
+      cb(null, user)
+
+    } catch(err) {
+      console.log(err)
+      res.send(err)
+    }
   }
 ));
 
@@ -20,7 +35,12 @@ passport.serializeUser(function(user, done) {
 });
 
 passport.deserializeUser(function(id, done) {
-
+  UserModel.findById(id)
+           .then(function(userDoc) {
+            done(null, userDoc)
+           }).catch(err => {
+            done(err)
+           })
   // Find your User, using your model, and then call done(err, whateverYourUserIsCalled)
   // When you call this done function passport assigns the user document to req.user, which will 
   // be availible in every Single controller function, so you always know the logged in user
