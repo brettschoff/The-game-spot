bacd074da74ec9b73be59baf37b5c174a078d8e4
brettschoff <brett.schoff@gmail.com>
2023-03-30 13:01:51 -0600

    Added the game show page with api.

--- controllers/games.js
+++ controllers/games.js
@@ -19,25 +19,37 @@ async function index(req, res) {
             client_secret: process.env.TWITCH_SECRET,
             grant_type: "client_credentials"
         })
-        const allGames = await axios.post('https://api.igdb.com/v4/games', {}, {
-            body: {
-                fields: 'name',
-                limit: 10
-            },
+        const allGames = await axios.post('https://api.igdb.com/v4/games', 
+            'fields *; limit 50;', 
+            {
             headers: {
                 'Client-ID': process.env.TWITCH_CLIENT_ID,
                 Authorization: 'Bearer ' + response.data.access_token
+
             }
         })
+        const ids = allGames.data.map(game => game.id);
+
+         // Make a new Axios request to retrieve the names using the IDs
+        const response2 = await axios.post('https://api.igdb.com/v4/games',
+            `fields name; where id=(${ids.join(',')});`,
+            {
+                headers: {
+                    'Client-ID': process.env.TWITCH_CLIENT_ID,
+                    Authorization: 'Bearer ' + response.data.access_token,
+                    'Content-Type': 'text/plain'
+        }
+      }
+    );
         
-        console.log(allGames, '<---- Should be names of games')
+        console.log(response2, '<---- Should be names of games')
         console.log(typeof(allGames.data))
 //         const allGames = await Game.find({}).sort('name')
 //         res.render('games/index', {
 //             games: allGames
 //         })
     res.render('games/index', {
-        games: allGames.data
+        games: response2.data
         }
     )
 
@@ -53,12 +65,44 @@ function newGame(req, res) {
 
 async function show(req, res) {
     try{
-        const oneGame = await Game.findById(req.params.id).populate('review').exec()
-        const gameReviews = oneGame.review
-        console.log(oneGame)
+        const response = await axios.post('https://id.twitch.tv/oauth2/token', {
+            client_id: process.env.TWITCH_CLIENT_ID,
+            client_secret: process.env.TWITCH_SECRET,
+            grant_type: "client_credentials"
+        })
+        const oneGame = await axios.post('https://api.igdb.com/v4/games', 
+        `fields *; where id=${req.params.id};`, 
+        {
+        headers: {
+            'Client-ID': process.env.TWITCH_CLIENT_ID,
+            Authorization: 'Bearer ' + response.data.access_token
+            }
+        })
+        const gameCover = await axios.post('https://api.igdb.com/v4/covers',
+            `fields url; where id=${oneGame.data[0].cover};`,
+            {
+                headers: {
+                    'Client-ID': process.env.TWITCH_CLIENT_ID,
+                    Authorization: 'Bearer ' + response.data.access_token
+                }
+            })
+        const gameDeveloperId = await axios.post('https://api.igdb.com/v4/involved_companies',
+        `fields company; where id=${oneGame.data[0].involved_companies[0]}`,
+        {
+            headers: {
+                'Client-ID': process.env.TWITCH_CLIENT_ID,
+                Authorization: 'Bearer ' + response.data.access_token
+            }
+        })
+        const gameDeveloper = await axios.post('https://api.igdb.com/v4/companies',
+        `fields name; where id=${gameDeveloperId.data[0].company}`)
+    console.log(gameDeveloperId.data[0])
+    console.log(gameCover.data[0])
+    console.log(oneGame.data)
         res.render('games/show', {
-            game: oneGame,
-            review: gameReviews
+            game: oneGame.data[0],
+            cover: gameCover.data[0],
+            developer: gameDeveloper.data[0]
         })
     } catch(err) {
         console.log(err)
